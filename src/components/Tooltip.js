6336b01b6bebdb7686ad755729b6f1a8120fe559
Arjun Singh <arjunsinghbazal@gmail.com>
2023-06-15 12:06:39 +0530

    submitting code-Thu Jun 15 2023 12:06:37 GMT+0530 (India Standard Time)

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Tool=({value,enter,leave,svalue,Enter,Leave})=>{
 return(
-    <div className="tooltiptext" >
-      <div onMouseEnter={enter} onMouseLeave={leave}><h1>Hover over me</h1> {value?<p>This is a Tooltip</p>:null}</div>
-      <div onMouseEnter={Enter} onMouseLeave={Leave}><h1>Hover over me to see another Tooltip</h1> {svalue?<p>This is a Tooltip of another toolkit</p>:null}</div>
+    <div >
+      <div className="tooltiptext" onMouseEnter={enter} onMouseLeave={leave}><h1>Hover over me</h1> {value?<p className="tootip">This is a Tooltip</p>:null}</div>
+      <div className="tooltiptext" onMouseEnter={Enter} onMouseLeave={Leave}><h1>Hover over me to see another Tooltip</h1> {svalue?<p className="tootip">This is a Tooltip of another toolkit</p>:null}</div>
     </div>
 )
 }
